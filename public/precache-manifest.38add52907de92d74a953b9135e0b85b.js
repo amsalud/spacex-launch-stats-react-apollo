27e9ab3baa4fb37a2dffedb7db1bc794e84b0c6e
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2019-03-17 00:35:03 -0400

    Prepared for Deployment

---
+++ public/precache-manifest.38add52907de92d74a953b9135e0b85b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4817300494f9be6e6458",
+    "url": "/static/css/main.78939aab.chunk.css"
+  },
+  {
+    "revision": "4817300494f9be6e6458",
+    "url": "/static/js/main.5d562441.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "92b5a578c643621c6fdd",
+    "url": "/static/js/2.8c7f846b.chunk.js"
+  },
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  },
+  {
+    "revision": "96b2a38ffe2a0084ba9ea2bb0031d1c1",
+    "url": "/index.html"
+  }
+];
