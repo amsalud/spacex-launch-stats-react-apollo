17a9dd7854f0436f23ceb6c9d92a4d6a9018ff11
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2019-03-16 20:58:46 -0400

    Add rockets query

--- schema.js
+++ schema.js
@@ -57,6 +57,29 @@ const RootQuery = new GraphQLObjectType({
           .then(res => res.data)
           .catch(err => console.log(err));
       }
+    },
+    rockets: {
+      type: new GraphQLList(RocketType),
+      resolve(parent, args) {
+        return axios
+          .get('https://api.spacexdata.com/v3/rockets')
+          .then(res => res.data)
+          .catch(err => console.log(err));
+      }
+    },
+    rocket: {
+      type: RocketType,
+      args: {
+        id: {
+          type: GraphQLString
+        }
+      },
+      resolve(parent, args) {
+        return axios
+          .get(`https://api.spacexdata.com/v3/rockets/${args.id}`)
+          .then(res => res.data)
+          .catch(err => console.log(err));
+      }
     }
   }
 });
