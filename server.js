1c66412fdb2be7d4ac74b6566b7f80cbdc469f40
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2019-03-16 22:43:25 -0400

    Update server.js

--- server.js
+++ server.js
@@ -14,4 +14,4 @@ app.use(
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Serer started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
